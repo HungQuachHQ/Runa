964a2b45d581857ae16842fa3550e0d988c8d5df
Hung Quach <161780888+HungQuachHQ@users.noreply.github.com>
2025-10-01 20:25:02 -0700

    Animated player movement
    Player animations done:
    - Idle animation
    - Walk animation
    - Jump animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,28 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    [SerializeField] private float speed;
     private Rigidbody2D body;
 
-    private void Awake() {
+    private float Move;
+
+    public float speed;
+    public float jump;
+
+    bool grounded;
+    private Animator animate;
+
+    private void Start() {
+        // Grabs references for the RigidBody and Animator from object
         body = GetComponent<Rigidbody2D>();
+        animate = GetComponent<Animator>();
     }
 
     private void Update() {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        Move = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(Move * speed, body.velocity.y);
 
         // Flips player wheb move left and right
-        if (horizontalInput > 0.01f) {
+        if (Move > 0.01f) {
             transform.localScale = Vector3.one;
         }
-        else if (horizontalInput < -0.01f) {
+        else if (Move < -0.01f) {
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && grounded) {
+            Jump();
+        }
+
+        // Setting Animator parameters
+        animate.SetBool("Walk", Move != 0);
+        animate.SetBool("Grounded", grounded);
+    }
+
+    private void Jump() {
+        body.velocity = new Vector2(body.velocity.x, jump);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Ground") {
+            Vector3 normal = collision.GetContact(0).normal;
+            if (normal == Vector3.up) {
+                grounded = true;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
         {
-            body.velocity = new Vector2(body.velocity.x, speed / 2);
+            grounded = false;
         }
     }
 }
